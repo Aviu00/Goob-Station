eb82c8aea8ea8ca75b5d156fb16a7c5e03669205
Aviu00 <aviu00@protonmail.com>
2025-11-26 22:26:10 +0300

    Fix.

--- Content.Shared/Chat/MsgChatMessage.cs
+++ Content.Shared/Chat/MsgChatMessage.cs
@@ -59,11 +59,12 @@ namespace Content.Shared.Chat
         public Color? MessageColorOverride;
         public string? AudioPath;
         public float AudioVolume;
+        public bool CanCoalesce; // Goob Edit
 
         [NonSerialized]
         public bool Read;
 
-        public ChatMessage(ChatChannel channel, string message, string wrappedMessage, NetEntity source, int? senderKey, bool hideChat = false, Color? colorOverride = null, string? audioPath = null, float audioVolume = 0)
+        public ChatMessage(ChatChannel channel, string message, string wrappedMessage, NetEntity source, int? senderKey, bool hideChat = false, Color? colorOverride = null, string? audioPath = null, float audioVolume = 0, bool canCoalesce = true) // Goob edit
         {
             Channel = channel;
             Message = message;
@@ -74,6 +75,7 @@ namespace Content.Shared.Chat
             MessageColorOverride = colorOverride;
             AudioPath = audioPath;
             AudioVolume = audioVolume;
+            CanCoalesce = canCoalesce; // Goob edit
         }
     }
 
